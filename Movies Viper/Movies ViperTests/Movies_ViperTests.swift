37fe0b690272fd803482fed6d70e4b1a222fda33
Gabriel D'Luca <gdsv@academy.cin.ufpe.br>
2020-06-15 14:18:49 -0300

    chore: add swiftlint to project

--- Movies Viper/Movies ViperTests/Movies_ViperTests.swift
+++ Movies Viper/Movies ViperTests/Movies_ViperTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import Movies_Viper
 
-class Movies_ViperTests: XCTestCase {
+class MoviesViperTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
